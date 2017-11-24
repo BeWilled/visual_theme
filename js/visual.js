d7716ad573587d4c3c9f8a2fdf2e8bb2b0996791
p-ok <none@gmail.com>
2017-11-24 17:08:39 -0500

    Side bar animations search animations layout setup

--- js/visual.js
+++ js/visual.js
@@ -3,8 +3,19 @@
   Drupal.behaviors.visual_js = {
     attach: function (context, settings) {
 
-      //Codigo aca
-
+      $('.content-layout .header .fa.fa-bars').once('clicked').click(function() {
+        $('.content-layout .header').toggleClass('clicked');
+      })
+      $(window).once('scrolled').on( 'DOMMouseScroll mousewheel', function ( event ) {
+        if( event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) { //alternative options for wheelData: wheelDeltaX & wheelDeltaY
+          $('.content-layout .header .header-inner .search-div').removeClass('opened');
+        } else {
+          $('.content-layout .header .header-inner .search-div').addClass('opened');
+        }
+      });
+      $('.content-layout .header .search-div .loop').once('clicked').click(function() {
+        $('.content-layout .header .header-inner .search-div').toggleClass('opened');
+      })
     }
   };
 }(jQuery));
