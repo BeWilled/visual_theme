9bfaa122a3ddcd673910446847d0e247ac905c17
p-ok <none@gmail.com>
2017-12-06 17:27:18 -0500

    overlay clicked

--- js/visual.js
+++ js/visual.js
@@ -4,7 +4,7 @@
     attach: function (context, settings) {
 
       $('.content-layout .header .fa.fa-bars').once('clicked').click(function() {
-        $('.content-layout .header').toggleClass('clicked');
+        $('.content-layout .header , .content-layout .overlay').toggleClass('clicked');
       })
       $(window).once('scrolled').on( 'DOMMouseScroll mousewheel', function ( event ) {
         if( event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) { //alternative options for wheelData: wheelDeltaX & wheelDeltaY
