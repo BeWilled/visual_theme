1ed7bfd17bb39f65ba493144f9099f3922280f02
p-ok <none@gmail.com>
2017-12-10 00:17:43 -0500

    Toggles in comments

--- js/visual.js
+++ js/visual.js
@@ -3,9 +3,16 @@
   Drupal.behaviors.visual_js = {
     attach: function (context, settings) {
 
-      $('.content-layout .header .fa.fa-bars').once('clicked').click(function() {
-        $('.content-layout .header , .content-layout .overlay').toggleClass('clicked');
-      })
+      $('.main-content .header .menu-button').once('clicked').click(function() {
+        $('.main-content .header , .content-layout .overlay').toggleClass('clicked');
+        $('.main-content .menu-button').toggleClass('clicked');
+
+      });
+
+      $('.content-video .comments .c-count').once('clicked').click(function() {
+        $('.content-video .add-comment').toggleClass('clicked');
+      });
+
       $(window).once('scrolled').on( 'DOMMouseScroll mousewheel', function ( event ) {
         if( event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) { //alternative options for wheelData: wheelDeltaX & wheelDeltaY
           $('.content-layout .header .header-inner .search-div').removeClass('opened');
